1da3dba96abfc8e924a36349d09080646bdc4e23
davidpangrib001 <96653161+davidpangrib001@users.noreply.github.com>
2022-04-04 19:17:39 +0700

    Update sourcecode.js

--- plugins/sourcecode.js
+++ plugins/sourcecode.js
@@ -1,8 +1,8 @@
 let fetch = require('node-fetch')
-let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'Source code')).buffer(), `
-Sc Nya Liat Di https://youtu.be/0wz53xj_5hI
-Jgn Lupa Di Subscribe yaa
-`.trim(), '❖ Kanao-Bot', 'Siap Banh🗿', 'Thanks')
+let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'SCRIPT')).buffer(), `
+Lagi Cari Script ya ??
+Nih script botnya..\nhttps://bit.ly/3J7jP1M\nPasscode : 36011
+`.trim(), '❖ One', 'Cara Lewatin-nya gimana ??', 'yntkts')
 handler.help = ['sourcecode']
 handler.tags = ['info']
 handler.command = /^(sourcecode|sc|scbot|script|github)$/i
