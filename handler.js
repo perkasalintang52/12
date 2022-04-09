f00d6a1ec85c67630d0f620a7cf02976632e5df2
davidpangrib001 <96653161+davidpangrib001@users.noreply.github.com>
2022-04-09 16:59:10 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -656,7 +656,7 @@ global.dfail = (type, m, conn) => {
     admin: '*Fitur Khusus Admin Grup*',
     nsfw: 'Perintah ini hanya bisa diaktifkan oleh owner',
   }[type]
-  if (msg) return conn.sendButton(m.chat, msg, watermark, '\nOke', 'Nanii', m)
+  if (msg) return conn.sendButton(m.chat, msg, watermark, 'Oke', 'Nanii', m)
   let botAdmin = {
     botAdmin: 'Mikir dkit lah bang *BOT NYA BUKAN ADMIN*',
   }[type]
@@ -664,7 +664,7 @@ global.dfail = (type, m, conn) => {
   let unreg = {
     unreg: `Silahkan daftar untuk menggunakan fitur ini dengan cara mengetik:\n\n*#daftar nama.umur*\n\nContoh: *#daftar David.16*`
   }[type]
-  if (unreg) return conn.sendButton(m.chat, `Halo kak ${name} Sebelum menggunakan fitur ini Harap Sign Up ( Daftar ) Terlebih dahulu`, watermark, 'Sign Up', `#daftar ${name}.17`, { key: { fromMe: false, remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net' }, message: { orderMessage: { message: `©shirobotz_\nJam ${time}`, itemCount: 2022, thumbnail: fs.readFileSync('./src/One.jpg')}}})
+  if (unreg) return conn.sendButton(m.chat, `Halo, ${name} Sebelum menggunakan fitur di bot, harap Sign Up ( Daftar ) Terlebih dahulu\nJika Button Tidak Merespon Daftar Manual:\n*.daftar Nama.Umur*\nContoh: *.daftar ${name}.15`, watermark, 'Sign Up', `#daftar ${name}.17`, { key: { fromMe: false, remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net' }, message: { orderMessage: { message: `OneBotz\nJam ${time}`, itemCount: 2022, thumbnail: fs.readFileSync('./src/Dark.jpg')}}})
 }
 
 let chalk = require('chalk')
