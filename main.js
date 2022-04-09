2a14ade78e6173af8ea94c884d1f0bc8f951644d
davidpangrib001 <96653161+davidpangrib001@users.noreply.github.com>
2022-04-09 16:57:20 +0700

    Update main.js

--- main.js
+++ main.js
@@ -43,7 +43,7 @@ global.DATABASE = global.db // Backwards Compatibility
 
 global.conn = new WAConnection()
 conn.version =[2,2143,3]
-conn.browserDescription =['Galaxy Team', 'Edge', '3.0']
+conn.browserDescription =['Colour Your Life', 'Firefox', '3.0']
 let authFile = `${opts._[0] || 'OneBotZ'}.json`
 if (fs.existsSync(authFile)) conn.loadAuthInfo(authFile)
 if (opts['trace']) conn.logger.level = 'trace'
