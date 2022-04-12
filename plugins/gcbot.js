450642e45835e67c619ee495563bd660d0f92af2
davidpangrib001 <96653161+davidpangrib001@users.noreply.github.com>
2022-04-12 08:57:23 +0700

    Update gcbot.js

--- plugins/gcbot.js
+++ plugins/gcbot.js
@@ -1,19 +1,17 @@
+// Tambahin Link Kalian Sendiri
 let fetch = require('node-fetch')
 let handler = async (m, { conn, text }) => {
   let ext= `
-╭═════════════════
-║╭──❉ [ GRUB BOT ] ❉──
-║│➸ *𝐆𝐑𝐎𝐔𝐏 𝐎𝐅𝐅𝐈𝐂𝐈𝐀𝐋 1*
-║│➸ https://tinyurl.com/One-Groups-1
-║│➸ *𝐆𝐑𝐎𝐔𝐏 𝐎𝐅𝐅𝐈𝐂𝐈𝐀𝐋 2*
-║│➸ https://tinyurl.com/One-Groups-2
-║│➸ *𝐆𝐑𝐎𝐔𝐏 𝐎𝐅𝐅𝐈𝐂𝐈𝐀𝐋 3*
-║│➸ https://tinyurl.com/One-Groups-3
-║╰───────────────
-╰══════════════════
-
+━━━━━━━━━━━━━━━
+_*Grup Official*_
+━━━━━━━━━━━━━━━
+┏━━━━━━━━━━━━━ꕥ
+┃ *_Grup Utama_*  : https://chat.whatsapp.com/GfwblXDHAqF4iA27wk8Gf4 => Ramein
+┃ *_Grup Kedua_*  : https://chat.whatsapp.com/EheAWPrQMhV25xW0N7l7WD
+┃ *_ Grup Ketiga_* : https://chat.whatsapp.com/GzDQlgoUamS5Ok4EOcYRRB
+┗━━━━━━━━━━━━━ꕥ
 `.trim()
-conn.send2ButtonLoc(m.chat, await (await fetch(fla + 'One Groups')).buffer(), ext, 'One', '\nSIAP BANG🗿', 'Iya', 'Owner', '.owner', m)
+conn.send2ButtonLoc(m.chat, await (await fetch(fla + 'One Groups')).buffer(), ext, 'One', 'SIAP BANG🗿', 'Iya', 'Owner', '.owner', m)
 
 }
 handler.help = ['gcbot']
