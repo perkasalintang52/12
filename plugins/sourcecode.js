baf74232743f92218e793240800c52c00f77b69f
davidpangrib001 <96653161+davidpangrib001@users.noreply.github.com>
2022-04-12 09:03:10 +0700

    Update sourcecode.js

--- plugins/sourcecode.js
+++ plugins/sourcecode.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
-let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'SCRIPT')).buffer(), `
+let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'Script')).buffer(), `
 Lagi Cari Script ya ??
-Nih script botnya..\nhttps://bit.ly/3J7jP1M\nPasscode : 36011
+Nih script botnya..\nMediafire : https://bit.ly/3usJcqJ ( For Termux )\nGithub : https://github.com/davidpangrib001/Hyzer-V3\n_*Jangan Lupa Kasih Starsnya ya..*_
 `.trim(), '❖ One', 'Cara Lewatin-nya gimana ??', 'yntkts')
 handler.help = ['sourcecode']
 handler.tags = ['info']
