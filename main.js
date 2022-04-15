535ae07d1a1513896203d0e3ddb6e0ea6e0937e7
OneBotz Owner <96653161+davidpangrib001@users.noreply.github.com>
2022-04-15 13:35:59 +0700

    Update main.js

--- main.js
+++ main.js
@@ -43,7 +43,7 @@ global.DATABASE = global.db // Backwards Compatibility
 
 global.conn = new WAConnection()
 conn.version =[2,2143,3]
-conn.browserDescription =['Saha OneBotz ?', 'Unknown', '3.0']
+conn.browserDescription =['Over the Horizon', 'Desktop', '3.0']
 let authFile = `${opts._[0] || 'OneBotZ'}.json`
 if (fs.existsSync(authFile)) conn.loadAuthInfo(authFile)
 if (opts['trace']) conn.logger.level = 'trace'
