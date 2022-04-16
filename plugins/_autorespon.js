94f5a895c4dc1e1488fa2d6afe14f9085e23f406
OneBotz Owner <96653161+davidpangrib001@users.noreply.github.com>
2022-04-16 14:41:19 +0700

    Update _autorespon.js

--- plugins/_autorespon.js
+++ plugins/_autorespon.js
@@ -58,7 +58,7 @@ handler.all = async function (m, { isBlocked }) {
                 year: 'numeric'
             })
             await global.db.write()
-            this.reply(global.owner[0] + '@s.whatsapp.net', `Database: ${date}`, null)
+            this.reply(global.owner[0] + '@s.whatsapp.net', `Database Tanggal ${date}`, null)
             this.sendFile(global.owner[0] + '@s.whatsapp.net', fs.readFileSync('./database.json'), 'database.json', '', 0, 0, { mimetype: 'application/json' })
             setting.backupDB = new Date() * 1
         }
@@ -68,7 +68,7 @@ handler.all = async function (m, { isBlocked }) {
 if (new Date() * 1 - setting.status > 1000) {
         let _uptime = process.uptime() * 1000
         let uptime = clockString(_uptime)
-        await this.setStatus(`⏰ 𝖱𝗎𝗇𝗍𝗂𝗆𝖾: ${uptime} | 🌀 𝖬𝗈𝖽𝖾: ${global.opts['self'] ? '𝖯𝗋𝗂𝗏𝖺𝗍𝖾' : setting.groupOnly ? 'Hanya Grup' : '𝖯𝗎𝖻𝗅𝗂𝖼'} | 𝖮𝗇𝖾 𝖡𝗈𝗍 𝖻𝗒 𝖣𝖺𝗏𝗂𝖽`).catch(_ => _)
+        await this.setStatus(`⏲️ 𝖱𝗎𝗇𝗍𝗂𝗆𝖾: ${uptime} | 🛰 𝖬𝗈𝖽𝖾: ${global.opts['self'] ? '𝖯𝗋𝗂𝗏𝖺𝗍𝖾' : setting.groupOnly ? 'Hanya Grup' : '𝖯𝗎𝖻𝗅𝗂𝖼'} | 𝖮𝗇𝖾 𝖡𝗈𝗍 𝖻𝗒 𝖣𝖺𝗏𝗂𝖽`).catch(_ => _)
         setting.status = new Date() * 1
     }
 
